6fe001c51867e7e645b94e6c7f7d0a0328276a44
Sverre Källberg <sverre.kallberg@elev.ga.ntig.se>
2024-08-30 13:29:29 +0200

    hjguiu

---
+++ Modellering/Program.cs
@@ -0,0 +1,13 @@
+﻿Soap theSoap = new Soap();
+Sweat theSweat = new Sweat();
+
+Console.WriteLine($"{theSoap.name} has {theSoap.health} HP");
+Console.WriteLine($"{theSweat.name} has {theSweat.health} HP and size {theSweat.size}");
+
+Console.WriteLine("ATTACK!");
+theSweat.health -= 10;
+
+Console.WriteLine($"{theSweat.name} now has {theSweat.health} health");
+
+Console.WriteLine("Press any key to continue...");
+Console.ReadKey();
---
+++ Modellering/Sweat.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Sweat
+{
+    public int health = 50;
+    public int size = 3;
+    public string name = "Sweat";
+}
+
