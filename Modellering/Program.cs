cac63db1cef63c2b80a79ebb33506a2eb2b6079b
Sverre Källberg <sverre.kallberg@elev.ga.ntig.se>
2024-09-06 12:50:29 +0200

    sigma

--- Modellering/Program.cs
+++ Modellering/Program.cs
@@ -1,13 +1,35 @@
 ﻿Soap theSoap = new Soap();
 Sweat theSweat = new Sweat();
 
-Console.WriteLine($"{theSoap.name} has {theSoap.health} HP");
-Console.WriteLine($"{theSweat.name} has {theSweat.health} HP and size {theSweat.size}");
+Console.WriteLine($"{theSoap.name} has {theSoap.attack} attack power");
+Console.WriteLine($"{theSweat.name} has {theSweat.health} health points");
 
-Console.WriteLine("ATTACK!");
-theSweat.health -= 10;
+// Console.WriteLine("ATTACK!");
+// theSweat.health -= 10;
 
-Console.WriteLine($"{theSweat.name} now has {theSweat.health} health");
+// Console.WriteLine($"{theSweat.name} now has {theSweat.health} health");
 
-Console.WriteLine("Press any key to continue...");
+// Console.WriteLine("Press any key to continue...");
+// Console.ReadKey();
+
+while (theSweat.health > 0)
+{
+    Console.WriteLine("\nWrite attack to damage Sweat");
+    
+    string damage = Console.ReadLine();
+    
+    if (damage == "attack")
+    {
+        theSweat.health -= theSoap.attack;
+    }
+
+    Console.WriteLine($"{theSweat.name} now has {theSweat.health} health points");
+
+    Console.WriteLine("Press any key to continue");
+
+    Console.ReadKey();
+}
+
+Console.WriteLine("\nCongratulations, the Sweat is now gone");
+Console.WriteLine("Press any key to close");
 Console.ReadKey();
--- Modellering/Sweat.cs
+++ Modellering/Sweat.cs
@@ -3,7 +3,6 @@ using System;
 public class Sweat
 {
     public int health = 50;
-    public int size = 3;
     public string name = "Sweat";
 }
 
